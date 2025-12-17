32c79ae5fa391bc8d54a6508e85b9ed23f9d6b64
GANESHARAJAN S <ssganesharajan@gmail.com>
2025-12-17 14:25:10 +0530

    Add files via upload

---
+++ day-6/INHERIT/inherit.js
@@ -0,0 +1,16 @@
+import Karna from './image';
+
+
+const App=()=>{
+  const x=58;
+  return(
+    <div>
+      <p>warrior</p>
+    <Karna />
+    <input type='text' id='ip'></input>
+    <button onClick="alertbox" type="submit">click me</button>
+    </div>
+  );
+}
+
+export default inherit;
